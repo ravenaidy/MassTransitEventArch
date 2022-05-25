b60a22a07470f81e8441883e95f28d3d291567cd
Alistair Ockhuis <Alistair.Ockhuis@derivco.co.za>
2022-05-25 15:54:59 +0200

    Updated Program and compose

--- MassTransitApi/Program.cs
+++ MassTransitApi/Program.cs
@@ -1,23 +1,18 @@
 using MassTransit;
 using MassTransitApi.Dto;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
-    c.SwaggerDoc("v1", new OpenApiInfo {Title = "MediatorApi", Version = "v1"})
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "MassTransitAPI", Version = "v1"})
 );
-
-builder.Services.AddMediator(cfg => { });
 
 builder.Services.AddMassTransit(bus =>
 {
